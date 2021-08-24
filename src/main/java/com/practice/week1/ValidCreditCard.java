a1448dc54432e8e8fdffd4f2e770cb2a292699c6
Kyle <kylea.castillo@calbaptist.edu>
2021-08-24 10:24:58 -0500

    Created a functioning version of Validating credit cards.

--- src/main/java/com/practice/week1/ValidCreditCard.java
+++ src/main/java/com/practice/week1/ValidCreditCard.java
@@ -1,15 +1,57 @@
 package com.practice.week1;
 
+/**
+ * A class that contains validation methods for credit cards.
+ * 
+ * @author Kyle Castillo
+ */
 public class ValidCreditCard {
-	
+
+	/**
+	 * Provides validation for a credit card to test if the card passes the Luhn
+	 * test.
+	 * 
+	 * @param card The card to be tested.
+	 * @return <code>true</code> if the card is valid <code>false</code> if not.
+	 */
 	public boolean validateCard(String card) {
-		if(card.length() >= 14 && card.length() <= 19) {
+		if (card.length() >= 14 && card.length() <= 19) {
 			// Do validation
-			return true;
-		
-		} else {
-			// The card does not meet the basic requirement to be a credit card. Return false.
-			return false;
-		}
+			// Remove the last digit, the check digit
+			Integer checkDigit = Integer.parseInt(card.substring(card.length() - 1));
+			int sum = 0;
+			String[] cardArr = card.split("");
+			// Reverse the number, IE check from the back and go to the front.
+			for (int i = cardArr.length - 2; i >= 0; i--) {
+				int testDigit = Integer.parseInt(cardArr[i]);
+				if(i % 2 != 0) {
+					testDigit *= 2;
+					if(testDigit > 10) {
+						while(testDigit > 0) {
+							sum += testDigit % 10;
+							testDigit /= 10;
+						}
+					} else {
+						sum += testDigit;
+					}
+				} else {
+					sum += testDigit;
+				}
+			}
+			
+			return checkDigit == 10 - (sum % 10) ? true : false;
+		} 
+		else return false;
+	}
+
+	/**
+	 * Provides validation for a credit card to test if the card passes the Luhn
+	 * test.
+	 * 
+	 * @param card The card to be tested.
+	 * @return <code>true</code> if the card is valid <code>false</code> if not.
+	 */
+	public boolean validateCard(Integer card) {
+		return validateCard(card.toString());
 	}
 }
--- src/test/java/com/practice/tests/ValidCreditCardTests.java
+++ src/test/java/com/practice/tests/ValidCreditCardTests.java
@@ -1,5 +1,6 @@
 package com.practice.tests;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -13,35 +14,55 @@ import com.practice.week1.ValidCreditCard;
 
 public class ValidCreditCardTests {
 	
-	private ValidCreditCard validCard;
+	private ValidCreditCard cv;
 	private static final Logger log = LoggerFactory.getLogger(ValidCreditCardTests.class);
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception{
-			log.info("Beginning tests for ValidCreditCard");
+			log.info("Beginning tests for ValidCreditCard.");
 			
 	}
 	
 	@Before
 	public void setupEmptyStringIsInvalid() {
-		log.info("Setting up credit card for emptyStringIsInvalid");
-		validCard = new ValidCreditCard();
+		log.info("Setting up ValidCreditCard object for testing.");
+		cv = new ValidCreditCard();
 	}
 	
 	@Test
 	public void emptyStringIsInvalid() {
-		log.info("Testing if an emtpy string returns false");
-		assertFalse(validCard.validateCard(""));
+		log.info("Testing if an emtpy string returns false.");
+		assertFalse(cv.validateCard(""));
+	}
+	
+	@Test
+	public void cardLengthMustBeBetween14And19() {
+		log.info("Testing if a card that is not between 14 and 19 characters is automatically invalid");
+		assertFalse(cv.validateCard("123"));
+		assertFalse(cv.validateCard("1234567890123"));
+		assertFalse(cv.validateCard("12345678901234567890"));
+	}
+	
+	@Test
+	public void testCardIsValid() {
+		log.info("Testing to see if a valid card returns as valid.");
+		assertTrue(cv.validateCard("1234567890123452"));
+	}
+	
+	@Test
+	public void testCardIsInvalid() {
+		log.info("Testing to see if an invalid card returns as invalid.");
+		assertFalse(cv.validateCard("123456789012345"));
 	}
 	
 	@After
 	public void teardownEmptyStringIsInvalid() {
-		log.info("Tearing down emptyStringIsInvalid");
-		validCard = null;
+		log.info("Tearing down ValidCreditCard object for testing.");
+		cv = null;
 	}
 	
 	@AfterClass
 	public static void teardownAfterClass() throws Exception{
-		log.info("Tests complete for ValidCreditCard");
+		log.info("Tests complete for ValidCreditCard.");
 	}
 }
