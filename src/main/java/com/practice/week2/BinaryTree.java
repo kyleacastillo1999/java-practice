285d74b3c06bd9b71ddd3d2a6e2767d2f48013dc
Kyle <kylea.castillo@calbaptist.edu>
2021-09-02 10:15:23 -0500

    Created a simple BinaryTree class

---
+++ src/main/java/com/practice/week2/BinaryTree.java
@@ -0,0 +1,48 @@
+package com.practice.week2;
+
+public class BinaryTree {
+	
+	private Node root;
+	
+	public BinaryTree(Node root) {
+		this.root = root;
+	}
+
+	public Node getRoot() {
+		return root;
+	}
+
+	public void setRoot(Node root) {
+		this.root = root;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((root == null) ? 0 : root.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BinaryTree other = (BinaryTree) obj;
+		if (root == null) {
+			if (other.root != null)
+				return false;
+		} else if (!root.equals(other.root))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "BinaryTree [root=" + root + "]";
+	}	
+}
